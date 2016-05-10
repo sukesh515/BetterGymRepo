c1cf5b4df545eb857d3b56072ebe0c9f52473582
sukeshw7 <sukeshw7@IBM-1feeee94d3d>
2016-05-10 10:30:26 +0100

    lll

---
+++ BetterGymWork/LoginBetter.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class LoginBetter {
+
+	public static void main(String[] args) {
+		
+				// TODO Auto-generated method stub
+
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http");
+		
+		
+	}
+
+}
