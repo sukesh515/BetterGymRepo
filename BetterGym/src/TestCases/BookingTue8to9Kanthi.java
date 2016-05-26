cc71f711bed240e103ed8ae7d96a5eacc301ae19
sukesh515 <sukesh515@yahoo.co.in>
2016-05-26 14:38:19 +0100

    Slight Changes in this class

--- BetterGym/src/TestCases/BookingTue8to9Kanthi.java
+++ BetterGym/src/TestCases/BookingTue8to9Kanthi.java
@@ -3,16 +3,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
-
-
-
-
-
-
-
-
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
